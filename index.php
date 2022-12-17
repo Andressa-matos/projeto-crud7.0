e080510efcfd8bd0b3eb1332041023dad3543af3
Andressa-matos <94369142+Andressa-matos@users.noreply.github.com>
2022-12-17 02:16:21 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="ar" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
+
+    <title>Cadastro</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Cadastro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=novo">Novo usuario</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=listar">Listar usuarios</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col mt-5">
+        <?php
+        include("novo-usuario.php");
+        switch(@$_REQUEST["page"]){
+            case "novo";
+        break;
+        case "listar";
+            include("listar-usuario.php");
+        break;
+
+        case "salvar";
+            include("salvar-usuario.php");
+        break;
+
+        case "editar";
+            include("editar-usuario.php");
+        break;
+
+        default:
+            print "<h1>bem-vindos!</h1>";
+        }
+    ?>
+
+        </div>
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
